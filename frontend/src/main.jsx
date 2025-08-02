22c946126e4ac7c1196d6f9f7359dd22029977e9
Kerolos Nabil <kerolosnabil247@gmail.com>
2025-08-02 22:14:23 +0300

    feat(frontend): implement the registration page
    Implemented the following:
      - Implement the register page structure.
      - Install the react router package.
      - Implement the routing between the login page and
      the register page.
    Closes #4

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,5 @@ import Register from './pages/Register.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Register></Register>
   </StrictMode>,
 )
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,81 +1,171 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Logo from '../assets/images/logo.png';
+import RegisterFooter from "../components/RegisterFooter";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 export default function Register() {
-    const [passHide, setPassHide] = useState(true);
-    const [confirmPassHide, setConfirmPassHide] = useState(true);
+  const [passHide, setPassHide] = useState(true);
+  const [confirmPassHide, setConfirmPassHide] = useState(true);
 
-    const handlePassword = () => {
-        setPassHide(!passHide);
-    };
+  const handlePassword = () => {
+    setPassHide(!passHide);
+  };
 
-    const handleConfirmPassword = () => {
-        setConfirmPassHide(!confirmPassHide)
-    }
+  const handleConfirmPassword = () => {
+    setConfirmPassHide(!confirmPassHide);
+  };
   return (
-    <div className='bg-[var(--primary-color)] w-full min-h-screen pb-4'>
-        <div className='flex flex-col justify-center items-center text-white mx-4 pt-8'>
-            <div className='pb-12'>
-                <img src="../../public/Logo.png" alt="" />
-                {/* <h1>hello</h1> */}
-            </div>
-
-            <form method='POST' className='w-full max-w-md flex flex-col gap-4 pt-4'>
-                {/* Container for first and second names */}
-                <div className='flex flex-col md:flex-row gap-4'>
-                    <div className='flex-1'>
-                        <label htmlFor="">First name</label>
-                        <input type="text" className='w-full h-10 border border-white rounded-md bg-transparent mt-1 pl-4' />
-                    </div>
-                    <div className='flex-1'>
-                        <label htmlFor="">Second name</label>
-                        <input type="text" className='w-full h-10 border border-white rounded-md bg-transparent mt-1 pl-4' />
-                    </div>
-                </div>
-
-                {/* Email, Password */}
-                <div>
-                    <label htmlFor="">Email address</label>
-                    <input type="text" className='w-full h-10 border border-white rounded-md bg-transparent mt-1 pl-4' />
-                </div>
-                <div>
-                    <label htmlFor="">Password</label>
-                    {/* container which have input field and the icon */}
-                    <div className='relative flex items-center'>
-                        <input type={passHide? "password": "text"} className='w-full h-10 border border-white rounded-md bg-transparent mt-1 pl-4' />
-                        <span className='absolute right-2 cursor-pointer' onClick={handlePassword}>
-                            {passHide? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-slash-fill" viewBox="0 0 16 16">
-                                <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z"/>
-                                <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/>
-                            </svg>
-                            :<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-fill" viewBox="0 0 16 16">
-                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
-                                <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
-                            </svg>}
-                        </span>
-                        
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="">Confirm password</label>
-                    {/* container which have input field and the icon */}
-                    <div className='relative flex items-center'>
-                        <input type={confirmPassHide? "password": "text"} className='w-full h-10 border border-white rounded-md bg-transparent mt-1 pl-4' />
-                        <span className='absolute right-2 cursor-pointer' onClick={handleConfirmPassword}>
-                            {confirmPassHide? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-slash-fill" viewBox="0 0 16 16">
-                                <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z"/>
-                                <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/>
-                            </svg>
-                            :<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-fill" viewBox="0 0 16 16">
-                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
-                                <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
-                            </svg>}
-                        </span>
-                    </div>
-                    
-                </div>
-            </form>
+    <div className="bg-[var(--primary-color)] w-full min-h-screen md:h-screen md:overflow-hidden flex flex-col overflow-hidden">
+      <div className="flex-grow flex flex-col justify-center items-center text-white mx-4 mb-2">
+        {/* Logo */}
+        <div className="flex justify-center ">
+          <img
+            src={Logo}
+            alt="Dropz Logo"
+            className="h-26 w-auto"
+          />
         </div>
-      
+        {/* container of the big card which hold the form */}
+        <div className="rounded-2xl shadow-lg p-4  bg-[var(--form-bg-color)] md:w-1/3 flex flex-col items-start">
+          <div>
+            <h1
+              className="text-left text-2xl font-bold tracking-tight text-white"
+              style={{ color: "var(--secondary-color)" }}
+            >
+              Welcome back,
+            </h1>
+            <h6 className="text-left text-sm tracking-tight text-white">
+              Sign up to continue
+            </h6>
+          </div>
+
+          {/* the form body */}
+          <div className="w-full flex justify-center">
+            <form
+              method="POST"
+              className="w-full max-w-md flex flex-col gap-1 pt-2 flex justify-center"
+            >
+              {/* Container for first and second names */}
+              <div className="flex flex-col md:flex-row gap-4">
+                <div className="flex-1">
+                  <label htmlFor="firstName">First name</label>
+                  <input
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    required
+                    className="w-full h-10 border border-white rounded-md bg-transparent mt-1 pl-4 focus:outline-none focus:ring-0"
+                  />
+                </div>
+                <div className="flex-1">
+                  <label htmlFor="secondName">Second name</label>
+                  <input
+                    type="text"
+                    id="secondName"
+                    name="secondName"
+                    required
+                    className="w-full h-10 border border-white rounded-md bg-transparent mt-1 pl-4 focus:outline-none focus:ring-0"
+                  />
+                </div>
+              </div>
+
+              {/* Email, Password */}
+              <div>
+                <label htmlFor="email">Email address</label>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  required
+                  className="w-full h-10 border border-white rounded-md bg-transparent mt-1 pl-4 focus:outline-none focus:ring-0"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="countries"
+                  className="block  text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Select a role
+                </label>
+                <select
+                  id="countries"
+                  className=" border border-white text-white text-sm rounded-lg focus:outline-none focus:ring-0 block w-full p-2.5"
+                >
+                  <option defaultValue={'Choose a role'} className="bg-[#39616c96] text-white">Choose a role</option>
+                  <option value="Customer" className="bg-[#39616c96] opacity-25 text-white">Customer</option>
+                  <option value="Seller" className="bg-[#39616c96] opacity-25 text-white">Seller</option>
+                  <option value="ShippingCompany" className="bg-[#39616c96] opacity-25 text-white">Shipping Company</option>
+                </select>
+              </div>
+              <div>
+                <label htmlFor="pass">Password</label>
+
+                {/* container which have input field and the icon */}
+                <div className="relative flex items-center">
+                  <input
+                    type={passHide ? "password" : "text"}
+                    id="pass"
+                    name="pass"
+                    required
+                    className="w-full h-10 border border-white rounded-md bg-transparent mt-1 pl-4 focus:outline-none focus:ring-0"
+                  />
+                  <span
+                    className="absolute right-2 cursor-pointer"
+                    onClick={handlePassword}
+                  >
+                    {passHide ? (
+                      <EyeSlashIcon className="h-5 w-5 text-white" />
+                    ) : (
+                      <EyeIcon className="h-5 w-5 text-white" />
+                    )}
+                  </span>
+                </div>
+              </div>
+              <div>
+                <label htmlFor="confirmPass">Confirm password</label>
+
+                {/* container which have input field and the icon */}
+                <div className="relative flex items-center">
+                  <input
+                    type={confirmPassHide ? "password" : "text"}
+                    id="confirmPass"
+                    name="confirmPass"
+                    required
+                    className="w-full h-10 border border-white rounded-md bg-transparent mt-1 pl-4 focus:outline-none focus:ring-0"
+                  />
+                  <span
+                    className="absolute right-2 cursor-pointer"
+                    onClick={handleConfirmPassword}
+                  >
+                    {confirmPassHide ? (
+                      <EyeSlashIcon className="h-5 w-5 text-white" />
+                    ) : (
+                      <EyeIcon className="h-5 w-5 text-white" />
+                    )}
+                  </span>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="w-full flex justify-center rounded-md bg-[var(--secondary-color)] px-4 py-2.5 text-sm font-semibold text-black shadow-sm hover:opacity-90"
+              >
+                Register
+              </button>
+            </form>
+          </div>
+          <p className="mt-5 text-center text-sm text-white w-full flex justify-center">
+            Already have an account ?{" "}
+            <Link
+              to="/login"
+              className="text-[var(--secondary-color)] font-medium hover:underline"
+            >
+              &nbsp;Login
+            </Link>
+          </p>
+        </div>
+      </div>
+      <RegisterFooter></RegisterFooter>
     </div>
   );
 }
