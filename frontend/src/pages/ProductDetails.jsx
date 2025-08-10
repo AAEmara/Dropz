3747ccfc612431bf858ff04681b0f13a7648bdb8
Kerolos Nabil <kerolosnabil247@gmail.com>
2025-08-11 02:30:48 +0300

    the product detials setup

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,1 +1,13 @@
-export default function ProductDetails() {}
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function ProductDetails() {
+  return (
+    <>
+        <Navbar/>
+        <h1>hellllo</h1>
+        <Footer/>
+    </>
+  )
+}
