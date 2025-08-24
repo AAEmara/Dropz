f0537cdd394eeed8f76ba33ab309fc4d6f8dde51
AAEmara <abdelrahmanemara18@gmail.com>
2025-08-24 05:24:53 +0300

    chore: add fixtures for different apps
    Added the following fixtures for:
      - Users
      - Addresses
      - Different Profiles such as:
        - CustomerProfile
        - SellerAccount
        - ShippingCompany
      - Product Categories
      - Products
    
    Added the load_scripts.sh script to load the fixtures
    for the previous features.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import hello_world
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -12,4 +14,4 @@ urlpatterns = [
     path("api/", include("carts.urls")),
     path("api/", include("wishlists.urls")),
     path("api/support/", include("support.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ scripts/load_scripts.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Load Users first
+sudo docker compose exec backend python3 manage.py loaddata users_data
+
+# Load Addresses Second
+sudo docker compose exec backend python3 manage.py loaddata addresses_data
+
+# Now, we are setting up different profiles
+sudo docker compose exec backend python3 manage.py loaddata profiles_data
+
+# Setting up product categories
+sudo docker compose exec backend python3 manage.py loaddata categories_data
+
+# Products are ready to be added for each corresponding seller
+sudo docker compose exec backend python3 manage.py loaddata products_data
