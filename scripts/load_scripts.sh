82cf6e84a4a9b5e051fc997f79f44445ee522ced
AAEmara <abdelrahmanemara18@gmail.com>
2025-08-25 23:41:37 +0300

    feat(products): add electronics products
    Added electronics products corresponding to
    different sellers that are specialized in this
    category.

--- scripts/load_scripts.sh
+++ scripts/load_scripts.sh
@@ -15,3 +15,4 @@ sudo docker compose exec backend python3 manage.py loaddata categories_data
 # Products are ready to be added for each corresponding seller
 sudo docker compose exec backend python3 manage.py loaddata men_fashion_products_data
 sudo docker compose exec backend python3 manage.py loaddata women_fashion_products_data
+sudo docker compose exec backend python3 manage.py loaddata electronics_products_data
